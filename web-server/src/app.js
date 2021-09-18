5860113944f51922a55d6f97ebb0c600911cc763
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-09-18 20:06:21 +0530

    setting up app(express)

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -3,11 +3,26 @@ const app=express();
 
 
 app.get('',(req,res)=>{
-    res.send("Hello i am Uttkarsh Raj")
+    res.send("Hello i am Uttkarsh Raj");
 })
 
 //app.com
 //app.com/help
 //app.com/about
 
-app.listen(3000);
+app.get('/help',(req,res)=>{
+    res.send("HELP PAGE");
+})
+
+app.get("/about",(req,res)=>{
+    res.send("IT's About page")
+})
+
+
+app.get("/weather",(req,res)=>{
+    res.send("Weather Page")
+})
+
+app.listen(3000,()=>{
+    console.log("Server is up");
+});
