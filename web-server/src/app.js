2a5cf0460f88be075d26cd771c59dee97f27940b
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-09-19 21:41:01 +0530

    Dynamic pages

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -1,14 +1,32 @@
 const path=require("path");
 const express=require("express");
 const exp = require("constants");
+const { title } = require("process");
 
-console.log(__dirname);
-console.log(path.join(__dirname,'../public'));
+
 const app=express();
-app.set("view engine","hbs");
+app.set('view engine','hbs');
 app.use(express.static(path.join(__dirname,'../public')));
 app.get('',(req,res)=>{
-    res.render('index');
+    res.render('index',{
+        title:'Weather App',
+        name:'Uttkarsh'
+    });
+ 
+})
+app.get('/about',(req,res)=>{
+    res.render('about',{
+        title:'Weather App',
+        name:'Uttkarsh'
+    });
+
+})
+app.get('/help',(req,res)=>{
+    res.render('help',{
+        helpText:"This is helpful text",
+       
+    });
+
 })
 
 // app.get('',(req,res)=>{
