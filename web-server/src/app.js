24cdea97aa9b16641dd8529a76e99a97c06b7e5c
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-09-20 14:40:27 +0530

    Advance Templating

--- web-server/src/app.js
+++ web-server/src/app.js
@@ -2,28 +2,31 @@ const path=require("path");
 const express=require("express");
 const exp = require("constants");
 const { title } = require("process");
-
+const hbs=require("hbs");
 
 const app=express();
 //Define path for express config
 
 app.use(express.static(path.join(__dirname,'../public')));
-const viewspath=path.join(__dirname,'../templates')
-
+const viewspath=path.join(__dirname,'../templates/views')
+const partials=path.join(__dirname,'../templates/partials')
 
 app.set('view engine','hbs');
 app.set('views',viewspath);
+hbs.registerPartials(partials);
 
 app.get('',(req,res)=>{
     res.render('index',{
+        helpText:"this is some helpful text",
         title:'Weather App',
-        name:'Uttkarsh'
+        name:'Uttkarsh Raj'
+        
     });
  
 })
 app.get('/about',(req,res)=>{
     res.render('about',{
-        title:'Weather App',
+        title:'Abount Me',
         name:'Uttkarsh'
     });
 
@@ -31,7 +34,8 @@ app.get('/about',(req,res)=>{
 app.get('/help',(req,res)=>{
     res.render('help',{
         helpText:"This is helpful text",
-       
+        title:"Help",
+        name:'Uttkarsh',
     });
 
 })
