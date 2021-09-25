6e78f61268bef39e1623887bab86b665eed74a42
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-09-25 23:55:21 +0530

    Browse HTTP request with fetch

--- web-server/public/java script/app.js
+++ web-server/public/java script/app.js
@@ -1,1 +1,20 @@
 console.log("hello i am utttkarsh");
+
+fetch ('https://puzzle.mead.io/puzzle').then((response)=>{
+    response.json().then((data)=>{
+        console.log(data);
+    })
+})
+
+
+fetch('http://localhost:3000/weather?address=!').then((response)=>{
+    response.json().then((data)=>{
+        if(data.error){
+            console.log(data.error);
+        }
+        else{
+             console.log(data.location);
+             console.log(data.forecast);
+        }
+    })
+})
