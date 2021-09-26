134d0182ddf20e64723aceb40ba3f2387d496124
uttkarsh2406 <74823692+uttkarsh2406@users.noreply.github.com>
2021-09-26 14:26:19 +0530

    Creating Search Form

--- web-server/public/java script/app.js
+++ web-server/public/java script/app.js
@@ -1,13 +1,30 @@
 console.log("hello i am utttkarsh");
 
-fetch ('https://puzzle.mead.io/puzzle').then((response)=>{
-    response.json().then((data)=>{
-        console.log(data);
-    })
-})
+// fetch ('https://puzzle.mead.io/puzzle').then((response)=>{
+//     response.json().then((data)=>{
+//         console.log(data);
+//     })
+// })
 
 
-fetch('http://localhost:3000/weather?address=!').then((response)=>{
+// fetch('http://localhost:3000/weather?address=!').then((response)=>{
+//     response.json().then((data)=>{
+//         if(data.error){
+//             console.log(data.error);
+//         }
+//         else{
+//              console.log(data.location);
+//              console.log(data.forecast);
+//         }
+//     })
+// })
+
+const weatherform=document.querySelector('form')
+const search=document.querySelector('input')
+weatherform.addEventListener('submit',(event)=>{
+    event.preventDefault()
+    const location=search.value;
+    fetch('http://localhost:3000/weather?address='+location).then((response)=>{
     response.json().then((data)=>{
         if(data.error){
             console.log(data.error);
@@ -17,4 +34,6 @@ fetch('http://localhost:3000/weather?address=!').then((response)=>{
              console.log(data.forecast);
         }
     })
+})
+
 })
